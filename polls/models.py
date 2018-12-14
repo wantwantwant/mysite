35bb2404842bf8d1af1113fbdaa458b7e368a9ce
shitoucheng <778042395@qq.com>
2018-12-14 19:09:02 +0800

    二次提交

--- polls/models.py
+++ polls/models.py
@@ -1,14 +1,29 @@
 from django.db import models
+from django.utils import timezone
+from datetime import datetime, timedelta
+
 
 # Create your models here.
 class Question(models.Model):
     question_text = models.CharField('问题内容', max_length=200)
-    pub_data = models.DateTimeField('发布时间')
+    pub_date = models.DateTimeField('发布时间')
+
+    def __str__(self):   # (了解)控制打印对象时输出信息
+        return self.question_text
+
+    def was_published_recently(self):
+        if timezone.now() - timedelta(days=1) <=self.pub_date:
+            return True
+        else:
+            return False
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField('选项内容', max_length=200)
     votes = models.IntegerField('投票数', default=0)
+
+    def __str__(self):
+        return self.choice_text
 
 """
 类被翻译成sql执行
